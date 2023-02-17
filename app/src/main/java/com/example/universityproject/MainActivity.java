77ca7f5a724315291d2dc661dc200d1d00b658ee
Alexandr <alex.pravdinn@gmail.com>
2023-02-17 18:09:41 +0300

    Toast Messages maked!

--- app/src/main/java/com/example/universityproject/MainActivity.java
+++ app/src/main/java/com/example/universityproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.universityproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"On Create",Toast.LENGTH_SHORT).show();
     }
+
+    @Override
+    protected void onStop() {
+        Toast.makeText(getApplicationContext(),"On stop Toast",Toast.LENGTH_SHORT).show();
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(),"On Destroy",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(),"On Pause",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(),"On Resume",Toast.LENGTH_SHORT).show();
+    }
+
+
 }
