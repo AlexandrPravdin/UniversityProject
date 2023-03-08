89cc99cfcb319c3c7631cfebf8b900efb0ce5d0a
AlexandrPravdin <alex.pravdinn@gmail.com>
2023-03-08 22:17:48 +0300

    Trying to make onSaveInstanceState🥹🥹

--- app/src/main/java/com/example/universityproject/MainFragment.java
+++ app/src/main/java/com/example/universityproject/MainFragment.java
@@ -9,7 +9,6 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.universityproject.databinding.FragmentMainBinding;
@@ -19,28 +18,31 @@ import com.example.universityproject.databinding.FragmentMainBinding;
 
 public class MainFragment extends Fragment {
     private final static String LAST_TEXT_ON_THE_SCREEN = "Popa";
+    private final static String LOG_ID = "logs";
     private String lastTextThatWasOnTheScreen;
     private FragmentMainBinding binding;
     View.OnClickListener listener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            //Intent i;
-            Log.i("ff", "Кнопка нажата");
+
             if (v.getId() == R.id.playButton) {
                 binding.txtView1.setText("PlayingRadio");
             } else if (v.getId() == R.id.settingsButton) {
                 binding.txtView1.setText("GoToSettings");
+
                 //Сохранность в SaveState
                 Bundle b = new Bundle();
                 b.putString("1", "GoToSettings");
                 onSaveInstanceState(b);
 
                 //Переход с фрагмента на фрагмент
-                FragmentManager fragmentManager = getParentFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction().setReorderingAllowed(true);
-                fragmentTransaction.setCustomAnimations(R.anim.slide_in, R.anim.base_out, R.anim.base_in, R.anim.slide_out);
-                fragmentTransaction.replace(R.id.main_activity_fragment_container, SettingsFragment.class, null);
-                fragmentTransaction.addToBackStack("FromSettingsFragment");
+                FragmentTransaction fragmentTransaction = getParentFragmentManager().
+                        beginTransaction().setReorderingAllowed(true).
+                        setCustomAnimations(R.anim.slide_in, R.anim.base_out,
+                                R.anim.base_in, R.anim.slide_out)
+                        .replace(R.id.main_activity_fragment_container,
+                                SettingsFragment.class, null)
+                        .addToBackStack("FromSettingsFragment");
                 fragmentTransaction.commit();
             }
         }
@@ -49,12 +51,19 @@ public class MainFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(LOG_ID, "Created");
+    }
 
-        if (savedInstanceState != null) {
-            lastTextThatWasOnTheScreen = savedInstanceState.getString(LAST_TEXT_ON_THE_SCREEN);
-        } else {
-            lastTextThatWasOnTheScreen = "newView";
-        }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(LOG_ID, "Resumed");
+    }
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        Log.d(LOG_ID, "onViewStateRestored");
+        super.onViewStateRestored(savedInstanceState);
     }
 
     @Nullable
@@ -69,6 +78,12 @@ public class MainFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        if (savedInstanceState != null) {
+            Log.i(LOG_ID, savedInstanceState.getString(LAST_TEXT_ON_THE_SCREEN));
+            lastTextThatWasOnTheScreen = savedInstanceState.getString(LAST_TEXT_ON_THE_SCREEN);
+        } else {
+            lastTextThatWasOnTheScreen = "newView";
+        }
         binding.txtView1.setText(lastTextThatWasOnTheScreen);
     }
 
@@ -76,6 +91,25 @@ public class MainFragment extends Fragment {
     public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putString(LAST_TEXT_ON_THE_SCREEN, outState.getString("1"));
+        Log.d(LOG_ID, "onSaveInstanceState");
+        Log.i(LOG_ID, outState.getString("1"));
     }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(LOG_ID, "Paused");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(LOG_ID, "Stopped");
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(LOG_ID, "Destroyed");
+        super.onDestroy();
+    }
 }
--- app/src/main/java/com/example/universityproject/SettingsFragment.java
+++ app/src/main/java/com/example/universityproject/SettingsFragment.java
@@ -9,6 +9,8 @@ import android.widget.EditText;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.universityproject.databinding.FragmentSettingsBinding;
 
@@ -17,11 +19,22 @@ public class SettingsFragment extends Fragment {
     private EditText PlainText;
     private FragmentSettingsBinding binding;
 
+    View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (v.getId() == binding.SaveButton.getId()) {
+                FragmentManager manager = getParentFragmentManager();
+                manager.popBackStack();
+            }
+
+        }
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = FragmentSettingsBinding.inflate(getLayoutInflater());
+        binding.SaveButton.setOnClickListener(listener);
     }
 
     @Nullable
