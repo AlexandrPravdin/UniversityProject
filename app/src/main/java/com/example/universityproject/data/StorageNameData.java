31bc34df69b8d50c0da54dcc52031b836de14384
AlexandrPravdin <alex.pravdinn@gmail.com>
2023-05-06 16:43:43 +0300

    SharedPreferences

--- app/src/main/java/com/example/universityproject/data/StorageNameData.java
+++ app/src/main/java/com/example/universityproject/data/StorageNameData.java
@@ -1,45 +1,18 @@
 package com.example.universityproject.data;
 
-import android.Manifest;
-import android.app.Activity;
 import android.content.Context;
-import android.content.pm.PackageManager;
-import android.os.Environment;
-
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import android.content.SharedPreferences;
 
 public class StorageNameData {
     Context context;
-    private File file;
+    private final SharedPreferences file;
 
     public StorageNameData(Context context) {
-        this.context = context;
-        if (ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-            file = new File(Environment.getExternalStorageDirectory(), "name.txt");
-        } else {
-            ActivityCompat.requestPermissions((Activity) context, new String[]
-                    {Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-        }
+        file = context.getSharedPreferences("name", Context.MODE_PRIVATE);
     }
 
     public void saveName(String name) {
-        if (ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-            try {
-                FileWriter writer = new FileWriter(file);
-                writer.write(name);
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else {
-            ActivityCompat.requestPermissions((Activity) context, new String[]
-                    {Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-        }
+        file.edit().putString("name", name).apply();
     }
 
 }
