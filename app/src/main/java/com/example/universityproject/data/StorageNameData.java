6dbc704199ce0c8b5135ffabce7ff82eb31e6e7c
AlexandrPravdin <alex.pravdinn@gmail.com>
2023-05-06 16:33:13 +0300

    ExternalStorage

--- app/src/main/java/com/example/universityproject/data/StorageNameData.java
+++ app/src/main/java/com/example/universityproject/data/StorageNameData.java
@@ -1,27 +1,44 @@
 package com.example.universityproject.data;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class StorageNameData {
-    private final File file;
+    Context context;
+    private File file;
 
     public StorageNameData(Context context) {
-        File directoryFile = context.getFilesDir();
-        file = new File(directoryFile, "name.txt");
+        this.context = context;
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            file = new File(Environment.getExternalStorageDirectory(), "name.txt");
+        } else {
+            ActivityCompat.requestPermissions((Activity) context, new String[]
+                    {Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
     }
 
     public void saveName(String name) {
-        try {
-            FileWriter writer = new FileWriter(file);
-            writer.write(name);
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            try {
+                FileWriter writer = new FileWriter(file);
+                writer.write(name);
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            ActivityCompat.requestPermissions((Activity) context, new String[]
+                    {Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
         }
     }
 
